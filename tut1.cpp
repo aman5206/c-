89f4bf785c0d65cb0437d412fae1f61760e2ad95
aman5206 <henry7t8@gmail.com>
2026-03-25 21:49:00 +0530

    third commit

--- tut1.cpp
+++ tut1.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 using namespace std;
+// int glo = 99;
+int main()
+{
 
-int main(){
-int a=3;
-int b=5;
-    cout<<"the value of a is"<<a<< ".\n the vslue of b is"<<b ;
+    
+        // int a=3;
+        // int b=5;
+        //     cout<<"the value of a is"<<a<< ".\n the vslue of b is"<<b ;
 
-return 0;
+    // int a, b, c, d;
+    // int glo = 45;
+    
+
+    // cout << "Enter the value of a :";
+    // cin >> a;
+
+    // cout << "enter the value of b :";
+    // cin >> b;
+    // c = a + b;
+    // cout << "the value of c is :" << c;
+    // cout << "the value of glo c is :" <<::glo<< endl;
+// cout<<::glo;
+
+
+
+float d=3.14;
+long double e =3.14;
+
+cout<<"the size of 34.4 is "<<sizeof (34.4)<<endl;
+cout<<"the size of 34.4f is "<<sizeof (34.4f)<<endl;
+cout<<"the size of 34.4l is "<<sizeof (34.4l)<<endl;
+cout<<"the size of 34.4F is "<<sizeof (34.4F)<<endl;
+cout<<"the size of 34.4L is "<<sizeof (34.4L)<<endl;
+
+
+    return 0;
 }
---
+++ tut6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    // int a=6,b=7;
+    // cout<<"the value of a==b is :"<<(a==b)<<endl;
+    // cout<<(a!=b)<<endl;
+    // cout<<(a>=b)<<endl;
+    // cout<<(a<=b)<<endl;
+    // cout<<(a<b)<<endl;
+    // cout<<(a>b)<<endl;
+
+    // /*logical operators
+    // in cpp*/
+    char s;
+    cout<<"Enter your name:";
+    cin>>s;
+    cout<<"Your name is:"<<s;
+
+    
+    return 0;
+}
